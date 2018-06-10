895e38f19c9a94080904365817dcd5b45b81cd32
Mikolaj Jagiello <mikigraf@wp.pl>
2018-06-10 11:39:10 +0200

    Zapytania SQL

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -4,6 +4,7 @@ import config.Database;
 import model.User;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -14,10 +15,22 @@ public class UserDao {
     // save
     public User save(User user) throws SQLException {
     //insert do dB
-        Connection connection = database.getConnection(); //pobranie połączenia
+        Connection connection = database.getConnection(); //1.pobranie połączenia
 
+        /*String sql = "INSERT INTO user (first_name, last_name, email) " +
+                "VALUES (" + user.getFirstName() +", " + user.getLastName()+", "+ user.getEmail()+")"; */ //to spowoduje SQL injection (potencjalnie, wpisaniem np w którąś metodę "; DROP DATABASE;"
         String sql = "INSERT INTO user (first_name, last_name, email) " +
-                "VALUES (" + user.getFirstName() +" "; //zapytanie
+                "Values (?,?,?)";//2.zapytanie
+
+        PreparedStatement statement = connection.prepareStatement(sql); //3.obiekt PreparedStatement pomoże uniknąć SQL injection
+
+        statement.setString(1, user.getFirstName());
+        statement.setString(2, user.getLastName());
+        statement.setString(3, user.getEmail()); //4. parametry zapytania
+
+        statement.executeUpdate();
+        //5.wykonanie zapytania do dB
+        return user;
 
     }
     // update
@@ -36,14 +49,14 @@ public class UserDao {
     public User findById (int id)throws SQLException{
         Connection connection = database.getConnection();
 
-
+        return null;
     }
 
     // findAll
     public List<User> findAll()throws SQLException{
         Connection connection = database.getConnection();
 
-
+        return null;
     }
 
 
